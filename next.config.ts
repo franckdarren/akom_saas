79f252c5bf7c9f4e2ddc31d5ce2f9303e45edb95
Franck Darren <57094162+franckdarren@users.noreply.github.com>
2026-07-09 14:50:16 +0100

    fix image

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: '**.openfoodfacts.org',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.openfoodfacts.net',
+      },
     ],
   },
 };
